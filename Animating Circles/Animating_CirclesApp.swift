9fe293a3c162c11e6a27f873a9ca8c76d12afa21
victormac1 <victorcognito@yahoo.com>
2021-11-09 11:48:42 +0100

    first commit

---
+++ Animating Circles/Animating_CirclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animating_CirclesApp.swift
+//  Animating Circles
+//
+//   Created by Victors Code on 9/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Animating_CirclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
